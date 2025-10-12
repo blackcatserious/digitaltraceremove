6d11d6c0bc5df661349b0e65ca588151c6a33cc6
Artur Ziganshin <zikzk1394@gmail.com>
2025-10-12 15:22:43 +0200

    Add command center activation labs section

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,8 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import { BrowserRouter } from 'react-router-dom'
+import { SpeedInsights } from '@vercel/speed-insights/react'
+import { Analytics } from '@vercel/analytics/react'
 import App from './App.tsx'
 import './index.css'
 
@@ -9,5 +11,7 @@ createRoot(document.getElementById('root')!).render(
     <BrowserRouter>
       <App />
     </BrowserRouter>
+    <SpeedInsights />
+    <Analytics />
   </StrictMode>,
 )
