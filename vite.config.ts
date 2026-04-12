ddc46c74850d7548ae1cb262f1b71b9ce1a5c3d1
Artur Ziganshin <zikzk1394@gmail.com>
2026-04-12 17:58:44 +0200

    Add prerender plugin config for crawlable static routes

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,34 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import PrerenderPlugin from 'vite-plugin-prerender'
 
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    PrerenderPlugin({
+      staticDir: 'dist',
+      routes: [
+        '/',
+        '/en',
+        '/en/',
+        '/fr',
+        '/fr/',
+        '/es',
+        '/es/',
+        '/en/pricing',
+        '/en/partners',
+        '/en/case-studies',
+        '/en/about',
+        '/en/terms',
+        '/en/privacy',
+        '/en/refund',
+      ],
+      renderer: new PrerenderPlugin.PuppeteerRenderer({
+        renderAfterTime: 4000,
+        headless: true,
+      }),
+    }),
+  ],
   build: {
     sourcemap: false,
     cssCodeSplit: true,
