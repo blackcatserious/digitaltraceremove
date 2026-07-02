46fe28901c4d8a82112cd5230ae3d2f3e5c2a157
Artur Ziganshin <zikzk1394@gmail.com>
2026-07-02 22:11:16 +0200

    feat: expose laboratory routes for Netlify deployment

--- src/main.tsx
+++ src/main.tsx
@@ -4,13 +4,21 @@ import { BrowserRouter } from 'react-router-dom'
 import { SpeedInsights } from '@vercel/speed-insights/react'
 import { Analytics } from '@vercel/analytics/react'
 import App from './App.tsx'
+import Laboratory from './Laboratory.tsx'
 import './index.css'
+
+const pathname = window.location.pathname.replace(/\/+$/, '') || '/'
+const isLaboratoryRoute = pathname === '/lab' || pathname === '/concepts'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <BrowserRouter>
-      <App />
-    </BrowserRouter>
+    {isLaboratoryRoute ? (
+      <Laboratory />
+    ) : (
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
+    )}
     <SpeedInsights />
     <Analytics />
   </StrictMode>,
